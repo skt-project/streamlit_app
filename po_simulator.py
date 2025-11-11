f7c9c6a26117d5a341c03369a31d923c13312641
skt-project <sktrysb@gmail.com>
2025-11-11 17:57:20 +0700

    Adjusted reject SKUs

--- po_simulator.py
+++ po_simulator.py
@@ -356,8 +356,8 @@ def main():
 
     # Hardcoded Reject List
     MANUAL_REJECT_SKUS = [
-        "G2G-34",
-        "G2G-72"
+        "G2G-252",
+        "G2G-253"
     ]
 
     # Additional rejected SKUs based on region rules
