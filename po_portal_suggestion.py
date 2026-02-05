bff4ea2ac189ce942c41458a096e83e32a6f3025
skt-project <sktrysb@gmail.com>
2026-02-05 12:01:14 +0700

    Update po_portal_suggestion.py

--- po_portal_suggestion.py
+++ po_portal_suggestion.py
@@ -88,7 +88,6 @@ def load_po_tracking():
         WHERE distributor_name LIKE '%KARYA ANANDA SUKSES%'
     """
     df = bq_client.query(query).to_dataframe()
-    df["order_date"] = pd.to_datetime(df["order_date"], errors="coerce")
     return df
 
 po_df = load_po_suggestion()
